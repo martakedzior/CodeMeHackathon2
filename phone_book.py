4a5efee09dfc15dd80e5b6056df4e287a80590f1
MartaKedzior <marta.kedzior@wp.pl>
2021-04-13 17:16:42 +0200

    Stage 2 of the exercise

--- phone_book.py
+++ phone_book.py
@@ -8,23 +8,54 @@ def save_file(filename, content):
     return 'file successfully saved'
 
 
+def new_entry(list):
+    user_input = input('Czy chcesz podać nowe dane do książki numerów? y/n: ')
+
+    new_entry = {
+    }
+
+    if user_input == 'y':
+        new_entry_name = input('Podaj imię: ')
+
+        while True:
+            try:
+                new_entry_number = int(input('Podaj numer: '))
+            except (ValueError):
+                print('To nie jest prawidłowa wartość! Spróbuj jeszcze raz!')
+                continue
+            break
+
+        new_entry[new_entry_name] = new_entry_number
+        list.append(new_entry)
+
+
+def read_entry_list():
+    filename = 'phone_book.json'
+
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    return data
+
+
 import json
 
-
 if __name__ == "__main__":
-
-    lista_numerow = []
-
-    wpis = {
+    number_list = []
+    entry = {
         'Anna Kowalska': 600000000,
         'Marta Kedzior': 500000000,
         'Marcin Grabowski': 501000000
     }
 
-    lista_numerow.append(wpis)
+    number_list.append(entry)
+    number_list_json = json.dumps(number_list)
+    filename = 'phone_book.json'
 
-    lista_numerow_json = json.dumps(lista_numerow)
+    save_file(filename, number_list_json)
 
-    filename = 'ksiazka_numerow.json'
+    new_entry(number_list)
+    number_list_json = json.dumps(number_list)
+    save_file(filename, number_list_json)
 
-    save_file(filename, lista_numerow_json)
+    read_entry_list()
