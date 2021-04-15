7e40f8a6256185126a3cf15971f21a9da0b040a8
MartaKedzior <marta.kedzior@wp.pl>
2021-04-15 18:37:49 +0200

    Final version of message generator

--- Exercise01/message_generator.py
+++ Exercise01/message_generator.py
@@ -1,6 +1,6 @@
 def save_file(filename, content):
     try:
-        with open(filename, 'x', encoding='UTF-8') as file:
+        with open(filename, 'w', encoding='UTF-8') as file:
             file.write(content)
     except FileExistsError:
         return 'not saved, file already exist'
@@ -8,7 +8,6 @@ def save_file(filename, content):
 
 
 def read_file(filename):
-    text = '***'
     try:
         with open(filename) as file:
             text = file.read()
@@ -21,22 +20,41 @@ def read_file(filename):
     return text
 
 
-def creating_messages(filename):
+def creating_data_structure(filename):
     with open(filename, 'r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
 
+        name_surname = []
+        exercise = []
+        grade_list = []
+        student_class_li = []
+
         for row in csvreader:
-            message = read_file('message.txt')
             name = row[1] + " " + row[2]
             task = row[3]
             grade = row[4]
+            if grade.isdigit() == False:
+                grade = 0
+
             student_class = row[0]
-            due_date = '30 June 2021'
 
-            message = message.format(name, student_class, task, grade, int(grade) + 1, due_date)
+            name_surname.append(name)
+            exercise.append(task)
+            grade_list.append(grade)
+            student_class_li.append(student_class)
 
-            filename = 'email_to_' + row[1] + "_" + row[2] + ".txt"
-            save_file(filename, message)
+    return name_surname, exercise, grade_list, student_class_li
+
+
+def creating_emails(name_surname, exercise, grade_list, student_class_li):
+    message = read_file('message.txt')
+    due_date = '30 June 2021'
+
+    for i in range(0,len(name_surname)-1):
+        message_to_save = message.format(name_surname[i],  student_class_li[i], exercise[i], grade_list[i], int(grade_list[i]) + 1, due_date)
+        filename = 'email_to_' + name_surname[i] + ".txt"
+        save_file(filename, message_to_save)
+        i += 1
 
 
 import csv
@@ -46,6 +64,7 @@ if __name__ == "__main__":
     filename = 'students.csv'
 
     try:
-        creating_messages(filename)
+        name_surname, exercise, grade_list, student_class_li = creating_data_structure(filename)
+        creating_emails(name_surname, exercise, grade_list, student_class_li)
     except FileExistsError:
-        print('file not found')
+        print('File not found')
